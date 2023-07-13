6e597f1fac9b677c7b4592023883b3bda859c502
Omar <iiobm127@gmail.com>
2023-07-13 13:38:39 +0300

    Added color wheel option

--- static/script.js
+++ static/script.js
@@ -1,44 +1,79 @@
 const DEF_COLOR = '#000000';
 const DEF_MODE = 'color';
 const DEF_SIZE = 16;
+const DEBOUNCE_DELAY = 20; // Delay in milliseconds for debounce
 
-let currentColor = DEF_COLOR
-let currentSize = DEF_SIZE
-let currentMode = DEF_MODE
+let currentColor = DEF_COLOR;
+let currentSize = DEF_SIZE;
+let currentMode = DEF_MODE;
 
-// Checking if mouse is pressed on the body
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+let mouseDown = false;
+document.body.addEventListener('mousedown', () => (mouseDown = true));
+document.body.addEventListener('mouseup', () => (mouseDown = false));
 
-const grid = document.getElementById('grid')
-function makeGrid(size){
-    // Clearing the grid
-    grid.innerHTML = '';
-    // Making the grid template
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+const grid = document.querySelector('#grid');
+const colorWheel = document.getElementById('colorWheel');
+const colorModeButton = document.getElementById('colorMode');
+const rainbowModeButton = document.getElementById('rainbowMode');
+const eraserModeButton = document.getElementById('eraserMode');
+const clearModeButton = document.getElementById('clearMode');
 
-    // Adding the squares
-    for (let i=0; i<size*size; i++){
-        const square = document.createElement('div')
-        // Adding event listeners to the squares
-        square.addEventListener('mouseover', changeColor)
-        square.addEventListener('mousedown', changeColor)
-        square.classList.add('square')
-        grid.appendChild(square)
-    }
+
+colorWheel.addEventListener('input', (event) => {
+  currentColor = event.target.value; // Update currentColor with selected color
+});
+
+colorWheel.oninput = (e) => setCurrentColor(e.target.value)
+
+function setColor (color){
+  currentColor = color
 }
 
-function changeColor(event){
-    if (event.type == 'mouseover' && !mouseDown) return
-    if (currentMode == 'color') {
-        event.target.style.backgroundColor = currentColor
-    }
+function makeGrid(size) {
+  // Clearing the grid
+  grid.innerHTML = '';
+
+  // Handling invalid size input
+  if (isNaN(size) || size < 1) {
+    size = DEF_SIZE;
+    console.error('Invalid size input. Using default size instead.');
+  }
+
+  // Making the grid template
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  // Adding the squares
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.addEventListener('mouseover', debounce(changeColor, DEBOUNCE_DELAY));
+    square.addEventListener('mousedown', debounce(changeColor, DEBOUNCE_DELAY));
+    square.classList.add('square');
+    grid.appendChild(square);
+  }
 }
 
-
-function clearGrid(){
-    grid.innerHTML = '';
+function changeColor(event) {
+  if (event.type === 'mouseover' && !mouseDown) return;
+  if (currentMode === 'color') {
+    event.target.style.backgroundColor = currentColor;
+  }
 }
-makeGrid(64);
+
+function clearGrid() {
+  grid.innerHTML = '';
+
+}
+
+makeGrid(32);
+
+// Debounce function
+function debounce(func, delay) {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
